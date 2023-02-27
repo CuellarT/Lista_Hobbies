fe442d35997f18ecd1112b850e482bd6350741a1
leonardo <leonardo@mail.equipo5.localhost>
2023-02-27 15:37:27 -0600

    iniciando lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  const name = "LEONARDO";
+  return  (
+  
+    <div>
+    <h3>Lista de Hobbies de {name}</h3>
+    <ul>
+      <li>Tocar guitarra</li>
+      <li>Jugar video juegos </li>
+      <li>escuchar musica </li>
+    </ul>
+    </div>  
+
+    )
+}
+
+export default App
