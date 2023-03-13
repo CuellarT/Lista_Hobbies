59611c54b89141fe19e2d6ce058252682dd17ce0
leonardo <leonardo@mail.equipo5.localhost>
2023-03-13 13:53:14 -0600

    Lista_Hobies

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-function App() {
-  const name = "LEONARDO";
-  return  (
-  
+import Footer from "./Footer";
+function App(){
+  const name = "LEONARDO FABIAN CUELLAR TEJADA";
+  return (
     <div>
-    <h3>Lista de Hobbies de {name}</h3>
-    <ul>
-      <li>Tocar guitarra</li>
-      <li>Jugar video juegos </li>
-      <li>escuchar musica </li>
-    </ul>
-    </div>  
-
-    )
+      <h3>Lista de Hobbies de {name}</h3>
+      <ul>
+        <li>Tocar guitarra</li>
+        <li>Jugar video juegos </li>
+        <li>escuchar musica </li>
+      </ul>
+      <hr />
+      <Footer/>
+    </div>
+  )
 }
-
-export default App
+export default App;
